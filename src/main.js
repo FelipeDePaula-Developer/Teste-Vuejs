9a9678a675e5c436311691a57a1506b8c15244ad
Felipe de Paula <fepsilva2015@hotmail.com>
2021-02-23 00:45:56 -0300

    iniciando back-end com PHP

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
-import VueRouter from "vue-router"
+import VueRouter from 'vue-router'
+import VueResorce from 'vue-resource'
 import Buefy from 'buefy'
 import App from './App.vue'
 import '../src/assets/main.scss'
 import router from './router'
 
+Vue.use(VueResorce)
 Vue.use(VueRouter)
 Vue.use(Buefy)
 Vue.config.productionTip = false
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,19 +1,36 @@
 <template>
   <div>
-      <section>
-          <b-field label="Email" :label-position="onBorder">
-              <b-input type="email"></b-input>
-          </b-field>
-          <b-field label="Senha" :label-position="onBorder">
-              <b-input type="password"></b-input>
-          </b-field>
-          <b-field label="Nome" :label-position="onBorder">
-              <b-input></b-input>
-          </b-field>
-      </section>
+    <section>
+      <form action="" method="post">
+        <b-field label="Email" :label-position="onBorder">
+          <b-input name="email" id="email" type="email"></b-input>
+        </b-field>
+        <b-field label="Senha" :label-position="onBorder">
+          <b-input name="senha" id="senha" type="password"></b-input>
+        </b-field>
+        <b-field label="Nome" :label-position="onBorder">
+          <b-input name="nome" id="nome" type="text"></b-input>
+        </b-field>
+        <b-button native-type="submit" id="btn-cadastrar" class="is-success" outlined> Cadastrar </b-button>
+      </form>
+    </section>
   </div>
 </template>
 <script>
+  document.querySelector("#btn-cadastrar").addEventListener("submit", async event =>{
+    event.preventDefault()
+
+    const email = document.querySelector("#email").innerHTML
+    // const senha = document.querySelector("#senha").value
+    // const nome = document.querySelector("#nome").value
+    axios.post("http://localhost:8082/",
+        {
+          email: `${email}`
+        }
+    )
+    
+  })
+
 export default {
   data() {
     return {
